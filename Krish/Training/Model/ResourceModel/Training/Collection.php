35caac0376482e45e2534b68df2fede1f72de9b8
Mayank <you@example.com>
2023-03-14 19:21:31 +0530

    column concat in grid

--- Krish/Training/Model/ResourceModel/Training/Collection.php
+++ Krish/Training/Model/ResourceModel/Training/Collection.php
@@ -8,9 +8,30 @@ use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
 
 class Collection extends AbstractCollection
 {
+    protected $_idFieldName = 'id';
     protected function _construct()
     {
         return $this->_init(Training::class, TrainingResourceModel::class);
+    }
+
+    protected function _initSelect()
+    {
+        parent::_initSelect();
+        $this->getSelect()->columns('CONCAT(`first_name`, " ", `last_name`) as full_name');
+        return $this;
+    }
+
+    public function addFieldToFilter($field, $condition = null)
+    {
+        if ($field === 'full_name') {
+            $conditionSql = $this->_getConditionSql(
+                'CONCAT(`main_table`.`first_name`, " ", `main_table`.`last_name`)',
+                $condition
+            );
+            $this->getSelect()->where($conditionSql);
+            return $this;
+        }
+        return parent::addFieldToFilter($field, $condition);
     }
 
 }
